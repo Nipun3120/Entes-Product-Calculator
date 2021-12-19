7d879831fbe422689cf05ddfb84c097978e1f3f4
Nipun Verma <Nipunverma39@gmail.com>
2021-12-19 14:45:10 +0530

    contractor model

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const apfcModel = require('../models/apfc');
+const contModel = require('../models/contractor');
 
 const route = express.Router();
 
@@ -16,7 +17,24 @@ route.post('/apfc', async (req, res)=> {
     try{
         const dbResponse = await newApfc.save();
         console.log(dbResponse)
-        res.status(201).json({'message': 'object created successfully'})
+        res.status(201).json({'message': 'apfc object created successfully'})
+    } catch (error) {
+        res.status(400).json({'error': error}).send('product insertion failed')
+    }
+    
+})
+route.post('/contractor', async (req, res)=> {
+    const newApfc = new contModel({
+        rating: req.body.rating,
+        model: req.body.model,
+        price: req.body.price,
+        discount: req.body.discount
+    })
+
+    try{
+        const dbResponse = await newApfc.save();
+        console.log(dbResponse)
+        res.status(201).json({'message': 'contractor object created successfully'})
     } catch (error) {
         res.status(400).json({'error': error}).send('product insertion failed')
     }
@@ -25,6 +43,13 @@ route.post('/apfc', async (req, res)=> {
 
 route.get('/apfc', async (req, res)=> {
     apfcModel.find({}, (err, products)=> {
+        if(err) res.status(400).json({'error': 'failed to fetch objects from database'})
+        else res.status(200).json(products)
+    })
+
+})
+route.get('/contractor', async (req, res)=> {
+    contModel.find({}, (err, products)=> {
         if(err) res.status(400).json({'error': 'failed to fetch objects from database'})
         else res.status(200).json(products)
     })
