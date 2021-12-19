1d15bb25b34bd040b6e60188c62cb8da54a55617
Nipun Verma <Nipunverma39@gmail.com>
2021-12-19 14:31:05 +0530

    routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,34 @@
 const express = require("express")
 const mongoose = require("mongoose");
+const apfcModel = require('../models/apfc');
 
 const route = express.Router();
 
-// route.post('/apfc', (req, res)=> {
-//     const newApfc = new 
-// })
+route.post('/apfc', async (req, res)=> {
+    const newApfc = new apfcModel({
+        type: req.body.type,
+        model: req.body.model,
+        noOfSteps: req.body.noOfSteps,
+        price: req.body.price,
+        discount: req.body.discount
+    })
 
+    try{
+        const dbResponse = await newApfc.save();
+        console.log(dbResponse)
+        res.status(201).send('success!')
+    } catch (error) {
+        res.status(400).json({'error': error}).send('product insertion failed')
+    }
+    
+})
+
+route.get('/apfc', async (req, res)=> {
+    apfcModel.find({}, (err, products)=> {
+        if(err) res.status(400).json({'error': 'failed to fetch objects from database'})
+        else res.status(200).json(products)
+    })
+
+})
 
 module.exports = route
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const httpServer = require("http");
 const cors = require('cors');
 const apfcModel = require('./models/apfc');
+const productsRoute = require('./routes/products');
 
 const app = express();
 const http = httpServer.createServer(app);
@@ -16,33 +17,7 @@ mongoose.connect("mongodb://localhost/entesdb", ()=> {
     error=> console.error(error)
 })
 
-
-app.post('/products/apfc', async (req, res)=> {
-    const newApfc = new apfcModel({
-        type: req.body.type,
-        model: req.body.model,
-        noOfSteps: req.body.noOfSteps,
-        price: req.body.price,
-        discount: req.body.discount
-    })
-
-    try{
-        const dbResponse = await newApfc.save();
-        console.log(dbResponse)
-        res.status(201).send('success!')
-    } catch (error) {
-        res.status(400).json({'error': error}).send('product insertion failed')
-    }
-    
-})
-
-app.get('/products/apfc', async (req, res)=> {
-    apfcModel.find({}, (err, products)=> {
-        if(err) res.status(400).json({'error': 'failed to fetch objects from database'})
-        else res.status(200).json(products)
-    })
-
-})
+app.use('/products', productsRoute)
 
 
 http.listen(PORT, ()=> console.log(`Server listening on port: ${PORT}`));
